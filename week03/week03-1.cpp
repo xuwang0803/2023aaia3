11bd62abd0b0e9db521520d9416e398f5ca5d770
xuwang0803 <144758478+xuwang0803@users.noreply.github.com>
2024-09-26 16:18:41 +0800

    week03

---
+++ week03/week03-1.cpp
@@ -0,0 +1,17 @@
+///code blocks-setting-compiler
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+        string s;
+        cin >> s;
+        for(char c:s)
+            {
+
+
+                cout << c <<"\n";
+            }
+
+}
+
---
+++ week03/week03-4.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        //https://cplusplus.com/reference/string/string/
+        return haystack.find(needle);
+    }
+};
