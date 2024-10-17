8abc843e358e58a275b8fea13a0a975c6acbe134
xuwang0803 <144758478+xuwang0803@users.noreply.github.com>
2024-10-17 16:37:22 +0800

    week06

---
+++ week06/week06-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+vector<int> a;
+int now;
+while( cin >> now )//Step01 Input
+{
+cout << now << "\n";//Step02 Output
+}
+}
