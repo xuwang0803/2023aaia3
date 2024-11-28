2e91ceba070014685ba6995fb5783a2ecf313d77
xuwang0803 <144758478+xuwang0803@users.noreply.github.com>
2024-11-28 16:33:16 +0800

    week12

---
+++ week12/week12-2.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        //ListNode* ans=list1;//宣告答案
+        ListNode* ans=new ListNode(99,new ListNode(88));;
+        //(3)新增一個值是99且右邊又 新增一個值是99
+        return ans;
+    }
+};
